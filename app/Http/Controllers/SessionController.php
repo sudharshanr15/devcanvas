aface85176a37e457e97fa147f9f58ebd4de5a22
Sudharshan <sudharshanrms2004@gmail.com>
2024-11-23 10:00:51 +0000

    Login and logout logics added

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -11,9 +13,25 @@ class SessionController extends Controller
     }
 
     public function store(){
-        echo "<pre>";
-        var_dump(request()->all());
-        echo "</pre>";
-        exit();
+        $validated = request()->validate([
+            "email" => ["required", "email"],
+            "password" => ["required"]
+        ]);
+
+        if(!Auth::attempt($validated)){
+            throw ValidationException::withMessages([
+                "email" => "Credentials unmatches"
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        return redirect("/");
+    }
+
+    public function destroy(){
+        Auth::logout();
+
+        return redirect("/");
     }
 }
