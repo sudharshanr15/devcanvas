7141fee8e58b73eb310391a52227f3ae60b1d8b1
Sudharshan <sudharshanrms2004@gmail.com>
2024-11-23 12:01:38 +0000

    Defined project routes

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,28 @@ Route::get('/', function () {
     return view('dashboard');
 })->middleware("auth");
 
+Route::middleware("auth")->prefix("/dashboard/{user}")->group(function(){
+    Route::get("/", function(){
+        return "sdf";
+    });
+
+    Route::get("/{project}", function(string $user, string $project){
+        return [$user, $project];
+    });
+
+    Route::get("/{project}/databases", function(string $user, string $project){
+        return [$user, $project, "databases"];
+    });
+
+    Route::get("/{project}/databases/{collection}", function(string $user, string $project, string $collection){
+        return [$user, $project, 'databases', "collections"];
+    });
+});
+
 Route::get("/register", [RegisteredUserController::class, "create"]);
 Route::post("/register", [RegisteredUserController::class, "store"]);
 
 Route::get("/login", [SessionController::class, "create"])->name("login");
 Route::post("/login", [SessionController::class, "store"]);
+
 Route::post("/logout", [SessionController::class, "destroy"]);
