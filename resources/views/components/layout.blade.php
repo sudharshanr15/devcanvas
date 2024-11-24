7742bc4409a2a81b234e073dcdc2046f69e012e6
Sudharshan <sudharshanrms2004@gmail.com>
2024-11-24 07:30:04 +0000

    Multiple column added for table creation

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 </head>
 <body class="">
     {{ $slot }}
--- resources/views/project/databases/collection/list.blade.php
+++ resources/views/project/databases/collection/list.blade.php
@@ -17,75 +17,14 @@
     <h2>Add</h2>
     <form action="" method="post">
         @csrf
-
         <div>
             <label for="table_name">Table Name</label>
             <input type="text" name="name" id="table_name" required>
         </div>
+        <div class="column-set">
 
-        <div>
-            <label for="column_name">Column Name</label>
-            <input type="text" name="columns[0][name]" id="column_name" required>
         </div>
-
-        <div>
-            <label for="column_type">Column Type</label>
-            <select name="columns[0][type]" id="column_type" required>
-                <option value="string">String</option>
-                <option value="integer">Integer</option>
-                <option value="text">Text</option>
-                <option value="boolean">Boolean</option>
-                <option value="float">Float</option>
-                <option value="date">Date</option>
-                <option value="datetime">Datetime</option>
-            </select>
-        </div>
-
-        <div>
-            <label for="nullable">Nullable</label>
-            <input type="checkbox" name="columns[0][nullable]" id="nullable" value="true">
-        </div>
-
-        <div>
-            <label for="default_value">Default Value</label>
-            <input type="text" name="columns[0][default]" id="default_value">
-        </div>
-
-        <div>
-            <label for="unique">Unique</label>
-            <input type="checkbox" name="columns[0][unique]" id="unique" value="true">
-        </div>
-
-        <div>
-            <label for="primary_key">Primary Key</label>
-            <input type="checkbox" name="columns[0][primary_key]" id="primary_key" value="true">
-        </div>
-
-        <div>
-            <label for="auto_increment">Auto Increment</label>
-            <input type="checkbox" name="columns[0][auto_increment]" id="auto_increment" value="true">
-        </div>
-
-        <fieldset>
-            <legend>Foreign Key</legend>
-            <div>
-                <label for="foreign_references">References Column</label>
-                <input type="text" name="columns[0][foreign_key][references]" id="foreign_references">
-            </div>
-            <div>
-                <label for="foreign_on">On Table</label>
-                <input type="text" name="columns[0][foreign_key][on]" id="foreign_on">
-            </div>
-            <div>
-                <label for="on_delete">On Delete Action</label>
-                <select name="columns[0][foreign_key][on_delete]" id="on_delete">
-                    <option value="">None</option>
-                    <option value="cascade">Cascade</option>
-                    <option value="set null">Set Null</option>
-                </select>
-            </div>
-        </fieldset>
-
+        <button type="button" onclick="addColumn()">Add column</button>
         <button type="submit">Create Table</button>
     </form>
     @if ($errors->any())
@@ -96,5 +35,78 @@
             @endforeach
         </ul>
     </div>
-@endif
+    @endif
+
+    <script>
+        let column_count = 0;
+        function addColumn(){
+            $(".column-set").append(`
+            <div>
+                <div>
+                    <label for="column_name">Column Name</label>
+                    <input type="text" name="columns[${column_count}][name]" id="column_name" required>
+                </div>
+
+                <div>
+                    <label for="column_type">Column Type</label>
+                    <select name="columns[${column_count}][type]" id="column_type" required>
+                        <option value="string">String</option>
+                        <option value="integer">Integer</option>
+                        <option value="text">Text</option>
+                        <option value="boolean">Boolean</option>
+                        <option value="float">Float</option>
+                        <option value="date">Date</option>
+                        <option value="datetime">Datetime</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label for="nullable">Nullable</label>
+                    <input type="checkbox" name="columns[${column_count}][nullable]" id="nullable" value="true">
+                </div>
+
+                <div>
+                    <label for="default_value">Default Value</label>
+                    <input type="text" name="columns[${column_count}][default]" id="default_value">
+                </div>
+
+                <div>
+                    <label for="unique">Unique</label>
+                    <input type="checkbox" name="columns[${column_count}][unique]" id="unique" value="true">
+                </div>
+
+                <div>
+                    <label for="primary_key">Primary Key</label>
+                    <input type="checkbox" name="columns[${column_count}][primary_key]" id="primary_key" value="true">
+                </div>
+
+                <div>
+                    <label for="auto_increment">Auto Increment</label>
+                    <input type="checkbox" name="columns[${column_count}][auto_increment]" id="auto_increment" value="true">
+                </div>
+
+                <fieldset>
+                    <legend>Foreign Key</legend>
+                    <div>
+                        <label for="foreign_references">References Column</label>
+                        <input type="text" name="columns[${column_count}][foreign_key][references]" id="foreign_references">
+                    </div>
+                    <div>
+                        <label for="foreign_on">On Table</label>
+                        <input type="text" name="columns[${column_count}][foreign_key][on]" id="foreign_on">
+                    </div>
+                    <div>
+                        <label for="on_delete">On Delete Action</label>
+                        <select name="columns[${column_count}][foreign_key][on_delete]" id="on_delete">
+                            <option value="">None</option>
+                            <option value="cascade">Cascade</option>
+                            <option value="set null">Set Null</option>
+                        </select>
+                    </div>
+                </fieldset>
+            </div>
+        `)
+        column_count++
+        }
+    </script>
 @endsection
